f0843036ff352928e046877ecfdeeaf5ab4843b4
Wirayudhas <arturwirayuda@gmail.com>
2024-06-04 10:55:43 +0700

    membuat perintah jika file ada di dalam directory

--- StreamReaderandWriter_0196.cpp
+++ StreamReaderandWriter_0196.cpp
@@ -34,4 +34,8 @@ int main() {
 	/*menunjuk ke sebuah file*/
 	infile.open("contohfile.txt");
 
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
 	
+
